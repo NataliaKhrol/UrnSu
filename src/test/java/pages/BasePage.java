4173d51a23ebc6a1bca67f47651f98543e5202cc
NatalyaKhrol <tashapas@yahoo.com>
2022-11-30 22:03:41 +0300

    New site to test started

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,28 @@
+package pages;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class BasePage {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    public static final String BASE_URL = "https://www.urn.su/qa/ui/basic_test/";
+
+    public BasePage(WebDriver driver){
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    }
+
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+}
---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,43 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.UnexpectedAlertBehaviour;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import pages.BasePage;
+
+public class BaseTest {
+    WebDriver driver;
+    BasePage basePage;
+
+    @Parameters({"browser"})
+    @BeforeMethod(description = "Opening the browser")
+    public void setup(@Optional("chrome") String browser, ITestContext context) {
+        if (browser.equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
+            options.setHeadless(true);
+            driver = new ChromeDriver(options);
+        } else if (browser.equals("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+        basePage = new BasePage(driver);
+    }
+
+    @AfterMethod(alwaysRun = true, description = "Closing the browser")
+    public void close() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
